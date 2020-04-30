35151ff1173f5103b36408abdeeb3da7b20e7fcb
Bs0Dd <63418512+Bs0Dd@users.noreply.github.com>
2020-04-30 09:52:16 +0600

    Add long_description_content_type

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ setup(
     description="simple wrapper for kundelik.kz API, based on pydnevnikruapi",
     install_requires=["requests", "aiohttp"],
     long_description=open("README.md", encoding="utf-8").read()
+    long_description_content_type="text/markdown",
 )
